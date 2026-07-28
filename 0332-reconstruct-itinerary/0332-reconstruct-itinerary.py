f60da9cf3ec53f3124c18e6ae5b0edbfd5e1f649
Sunkari Maheswari <sunkarimaheswari123@gmail.com>
2026-07-28 09:44:36 +0530

    Time: 3 ms (76.37%), Space: 20 MB (27.57%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        adj = defaultdict(list)
+        
+        for src, dst in sorted(tickets, reverse=True):
+            adj[src].append(dst)
+            
+        itinerary = []
+        
+        def dfs(airport):
+            while adj[airport]:
+                next_destination = adj[airport].pop()
+                dfs(next_destination)
+            itinerary.append(airport)
+            
+        dfs("JFK")
+        return itinerary[::-1]
